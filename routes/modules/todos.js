99d882bb754908f42083dd120c4c5b0a3a6b49a1
Sunnylin0320 <sunny8503201@gmail.com>
2023-07-29 19:52:05 +0800

    feat: add update todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -30,4 +30,33 @@ router.get("/:id", (req, res) => {
   
 });
 
+router.get("/:id/edit", (req, res) => {
+  const UserId = req.user.id;
+  const id = req.params.id;
+
+  return Todo.findOne({
+    where: { id, UserId },
+  })
+    .then((todo) => res.render("edit", { todo: todo.toJSON() }))
+    .catch((err) => console.log(err));
+});
+
+// 編輯todo
+router.put("/:id", (req, res) => {
+  const UserId = req.user.id;
+  const id = req.params.id;
+  const { name, isDone } = req.body;
+
+  return Todo.findOne({
+    where: { id, UserId },
+  })
+    .then((todo) => {
+      todo.name = name;
+      todo.isDone = isDone === "on";
+      return todo.save();
+    })
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
+});
+
 module.exports = router;
