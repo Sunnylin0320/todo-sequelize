bae07d1cba2796a0cb650ec35aa7afdf8dad3aea
Sunnylin0320 <sunny8503201@gmail.com>
2023-07-29 18:38:43 +0800

    feat: add relations between user & todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,10 +5,16 @@ const db = require("../../models");
 const Todo = db.Todo;
 
 router.get("/", (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true,
-  })
+  const UserId = req.user.id;
+  User.findByPk(UserId)
+    .then((user) => {
+      if (!user) throw new Error("User not found.");
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: { UserId },
+      });
+    })
     .then((todos) => res.render("index", { todos }))
     .catch((err) => console.log(err));
 });
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -5,13 +5,15 @@ const db = require("../../models");
 const Todo = db.Todo;
 
 router.get("/:id", (req, res) => {
+  const UserId = req.user.id;
   const id = req.params.id;
-  return (
-    Todo.findByPk(id)
+  return Todo.findOne({
+    where: { id, UserId }
+  })
       // 資料轉換成plain object 只需要在傳入樣板前加上toJSON()
       .then((todo) => res.render("detail", { todo: todo.toJSON() }))
-      .catch((err) => console.log(err))
-  );
+      .catch((err) => console.log(err));
+  
 });
 
 module.exports = router;
